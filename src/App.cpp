4cb401c3ff36604fc3ad03dccb901dd8440fb1c9
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-08-13 19:20:22 -0300

    chore

---
+++ src/App.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class App
+{
+private:
+    vector<string> message;
+
+public:
+    void start()
+    {
+        message = {"Henlo", "from", "C/C++", "project", "template!"};
+
+        for (const string &m : message)
+        {
+            cout << m << " ";
+        }
+
+        cout << endl;
+    }
+};
+
+int main()
+{
+    App app;
+
+    app.start();
+}
