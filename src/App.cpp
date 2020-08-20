5c78d0fb36980bdb199c2ad5bb5ea846fd0cf0fd
Traumerei <traumerei@pop-os.localdomain>
2020-08-20 19:21:00 -0300

    chore: formatting

--- src/App.cpp
+++ src/App.cpp
@@ -1,23 +1,24 @@
-
 #include <initializer_list>
 #include <stdio.h>
 
 class App
 {
 public:
-    void start() noexcept
+  void start() noexcept
+  {
+    const std::initializer_list<const char *> message{"Henlo", "from", "C/C++", "project", "template!"};
+
+    for (auto const &m : message)
     {
-        const std::initializer_list<const char*> message { "Henlo", "from", "C/C++", "project", "template!" };
-
-        for (auto const& m : message)
-            printf("%s ", m);
-
-        printf("\n");
+      printf("%s ", m);
     }
+
+    printf("\n");
+  }
 };
 
 int main()
 {
-    static App app;
-    app.start();
+  static App app;
+  app.start();
 }
